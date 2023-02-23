2b5489c9d77cda73c63516854f809a497391fa13
许晓良 <xuxiaoliang@xuxiaolangdeMBP.lan>
2023-02-23 09:31:04 +0800

    小程序init

---
+++ taroDemo/src/app.config.ts
@@ -0,0 +1,12 @@
+export default defineAppConfig({
+  //页面新增和删除对应pages
+  pages: [
+    'pages/index/index'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black'
+  }
+})
